caf827c44e28ded59a9a130ce6ea046208bade6e
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-09 01:16:00 +0200

    added image - grid - need to center the div - fix grid

--- crafted-plans/src/App.js
+++ crafted-plans/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contact from './HomePage/Contact';
 import Footer from './HomePage/Footer';
 import NavBar from './HomePage/NavBar';
+import Gallery from './HomePage/Gallery';
 
 function App() {
   return (
    <div>
     <Router>
       <Routes>
-      <Route path="/" element={ <> <NavBar/><Footer/> </>} />
+      <Route path="/" element={ <> <NavBar/><Gallery/><Footer/> </>} />
       <Route path="/contact" element={ <> <Contact/> </>} />
 
       </Routes>
