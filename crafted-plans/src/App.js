f6edf7a0a590347036138a536e3b3106e06be907
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-08 23:04:32 +0200

    Done with ProductsHomePage

--- crafted-plans/src/App.js
+++ crafted-plans/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contact from './HomePage/Contact';
 import Footer from './HomePage/Footer';
 import NavBar from './HomePage/NavBar';
+import ProductsHomePage from './HomePage/ProductsHomePage';
 
 function App() {
   return (
    <div>
     <Router>
       <Routes>
-      <Route path="/" element={ <> <NavBar/><Footer/> </>} />
+      <Route path="/" element={ <> <NavBar/><ProductsHomePage/><Footer/> </>} />
       <Route path="/contact" element={ <> <Contact/> </>} />
 
       </Routes>
---
+++ crafted-plans/src/HomePage/ProductsHomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../Styles/ProductsHomePage.css';
+import { Link } from 'react-router-dom';
+
+const ProductsHomePage = () => {
+  return (
+    <div>
+     <div className="products-home">
+      <div className="product-H">
+        <div className="line-home"></div>
+        <img src="/Images/image 1.png" alt="planner" className="product-img-home" />
+        <h4 className="product-name-home">Planners</h4>
+        <Link to='/planners' className='product-link-home'>Build Now</Link>
+      </div>
+      <div className="product-H">
+        <div className="line-home"></div>
+        <img src="/Images/image 2.png" alt="notepad" className="product-img-home" />
+        <h4 className="product-name-home">NotePads</h4>
+        <Link to='/notepads' className='product-link-home'>Shop Now</Link>
+      </div>
+      <div className="product-H">
+        <div className="line-home"></div>
+        <img src="/Images/image 3.png" alt="accessorie" className="product-img-home" />
+        <h4 className="product-name-home">Accessories</h4>
+        <Link to='/accessories' className='product-link-home'>Shop Now</Link>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default ProductsHomePage
