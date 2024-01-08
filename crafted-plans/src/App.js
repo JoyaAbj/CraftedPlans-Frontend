08ea8b4471ea61736010feca1a566515b50c39a7
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-09 01:23:18 +0200

    done with advantages

--- crafted-plans/src/App.js
+++ crafted-plans/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contact from './HomePage/Contact';
 import Footer from './HomePage/Footer';
 import NavBar from './HomePage/NavBar';
+import Advantages from './HomePage/Advantages';
 
 function App() {
   return (
    <div>
     <Router>
       <Routes>
-      <Route path="/" element={ <> <NavBar/><Footer/> </>} />
+      <Route path="/" element={ <> <NavBar/><Advantages/><Footer/> </>} />
       <Route path="/contact" element={ <> <Contact/> </>} />
 
       </Routes>
---
+++ crafted-plans/src/HomePage/Advantages.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/Advantages.css';
+
+const Advantages = () => {
+  return (
+    <div>
+      <div className="cont-home">
+        <h4 className="advantages-home">Free Shipping</h4>
+        <h4 className="advantages-home">Free Personalization</h4>
+        <h4 className="advantages-home">100% Guaranteed</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Advantages
