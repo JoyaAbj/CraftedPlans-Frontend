8f0716482fdef63506821e35026263ab4cf3bfef
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-08 21:54:20 +0200

    Done with Header

--- crafted-plans/src/App.js
+++ crafted-plans/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contact from './HomePage/Contact';
 import Footer from './HomePage/Footer';
 import NavBar from './HomePage/NavBar';
+import Header from './HomePage/Header';
 
 function App() {
   return (
    <div>
     <Router>
       <Routes>
-      <Route path="/" element={ <> <NavBar/><Footer/> </>} />
+      <Route path="/" element={ <> <NavBar/><Header/> </>} />
       <Route path="/contact" element={ <> <Contact/> </>} />
 
       </Routes>
