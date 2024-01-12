8202fa14c6e1a761e0e5338362806bfcdecdab6e
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-13 00:46:46 +0200

    done with information static

--- crafted-plans/src/Planner/Information.jsx
+++ crafted-plans/src/Planner/Information.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Information = () => {
   return (
-    <div>
-      Information
+    <div className='information-information'>
+      <div className="page-information">
+        <h2 className="title-information">
+          Information
+        </h2>
+        <p className="text-information">
+        Welcome to your planner.  Feel free to fill your first page
+        </p>
+        <form action="" className="form-information">
+          <input type="text"
+          className='input-information'
+          name="Full Name"
+          value="Full Name"
+          required
+          />
+          <input type="text"
+          className='input-information'
+          name="Phone Number"
+          value="Phone Number"
+          required
+          />
+          <input type="text"
+          className='input-information'
+          name="Email"
+          value="Email"
+          required
+          />
+          <textarea 
+          className='input-information'
+          name="" id="" 
+          cols="30" 
+          rows="10">
+          </textarea>
+        </form>
+      </div>
     </div>
   )
 }
