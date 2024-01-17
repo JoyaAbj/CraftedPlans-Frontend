ef4aa303657e246dad88ace8fe4c917fe7faac68
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-17 16:50:44 +0200

    fixing product-detail style

--- crafted-plans/src/HomePage/ProductAccessories.jsx
+++ crafted-plans/src/HomePage/ProductAccessories.jsx
@@ -58,9 +58,10 @@ const NotepadProduct = () => {
             <p className="product-desc-product">
             {accessorie.description} 
             </p>
+            <p className="product-desc-product">Quantity: {accessorie.quantity}</p>
             <div className="qty-cart">
             <div className="quantity-product">
-            <p className='product-quantity-product'>Quantity: {accessorie.quantity}</p>
+            <p className='product-quantity-product'>Quantity: {quantity}</p>
             <div className="btns-quantity">
             <button className='product-increase'
             onClick={handleDecrease}>-</button>
