6d22b69a196155e0642e1d307842c2a89f76ff0e
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-19 22:28:35 +0200

    added show mockup in pages

--- crafted-plans/src/Planner/Pages.jsx
+++ crafted-plans/src/Planner/Pages.jsx
@@ -6,6 +6,7 @@ const Pages = () => {
   const [selectedPlanner, setSelectedPlanner] = useState('weekly'); // Default to weekly planner
   const category = "pages";
   const [addOnsObj1, setAddOnsObj1] = useState({});
+  const [largeImage, setLargeImage] = useState('');
 
   const getAllPages = () => {
     axios.post(`http://localhost:5000/templates/getTemplateByCategory`, { category })
@@ -35,7 +36,21 @@ const Pages = () => {
       return template.name.toLowerCase().includes('daily planner');
     }
   });
+  const correspondingImageUrls = [
+    { name: "Weekly Planners", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FWeekly%20%20planner%20option%201.1.png?alt=media&token=a6c553fc-589d-497a-aaa9-3d30edb941ce" },
+    { name: "Weekly Planner-1", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FWeekly%20%20planner%20option%201.2.png?alt=media&token=d8e1e334-93b8-4bcd-bd6d-590fa3376d7d" },
+    { name: "Weekly Planner-2", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FWeekly%20%20planner%20option%201.3.png?alt=media&token=4ff5b981-c8fb-4762-aabb-7273d8101dce" },
   
+    {name: "Daily Planners", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FDaily%20%20planner%20option%201.1.png?alt=media&token=b1fb38b3-3a53-4f84-8ddf-79dcecf18c48"},
+    {name: "Daily Planner-1", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FDaily%20%20planner%20option%201.2.png?alt=media&token=dda242ff-8c82-4c05-9789-6205e558c866"},
+    {name: "Daily Planner-2", url: "https://firebasestorage.googleapis.com/v0/b/crafted-plans.appspot.com/o/files%2FDaily%20%20planner%20option%201.3.png?alt=media&token=da7277c4-b468-4478-a913-2fefa8b86d60"},
+];
+  const handleImageClick = (selectedImageName) => {
+    console.log(selectedImageName)
+    const result = correspondingImageUrls.filter(option=>option.name === selectedImageName)
+    console.log(result)
+    setLargeImage(result[0].url)
+  }
   const handleChange = (event, template) => {
     const templateId = template._id;
   
@@ -73,6 +88,14 @@ const Pages = () => {
 
   return (
     <div className='Dates'>
+       {/* Display the corresponding image */}
+     {largeImage !=='' && (
+        <img
+          src={largeImage}
+          alt="Corresponding Image"
+          className="corresponding-image"
+        />
+      )}
       <div className="toggle-btns-pages">
         <button
           className={`weekly-daily-btns ${selectedPlanner === 'daily' ? 'active-daily' : ''}`}
@@ -94,6 +117,7 @@ const Pages = () => {
               src={template.image[0]}
               alt={template.name}
               className="choose-template"
+              onClick={() => handleImageClick(template.name)}
             />
             <label class="labels-dates-m-s">
                       <input 
