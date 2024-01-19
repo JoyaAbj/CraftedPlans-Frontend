c486c223de9dc4cd853358d4736554cf2da77adc
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-20 01:10:04 +0200

    started login/register

--- crafted-plans/src/HomePage/Login.jsx
+++ crafted-plans/src/HomePage/Login.jsx
@@ -2,8 +2,63 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      login here
+    <div className='login/register'>
+        <h1 className="login-title">Login</h1>
+        <div className="login-btns-toggle">
+          <button className="login-button">Login</button>
+          <button className="login-button">Sign Up</button>
+        </div>
+      <div className="login-container">
+        <form className="login-form">
+          <label className="label-login">
+            <input 
+            type="text" 
+            placeholder='Email'
+            value="Email"
+            className="input-login" />
+          </label>
+          <label className="label-login">
+            <input 
+            type="password" 
+            placeholder='Password'
+            value="Password"
+            className="input-login" />
+          </label>
+          <input type="submit" className="login-submit" />
+        </form>
+      </div>
+      <div className="register-container">
+          <form  className="register-form">
+          <label className="label-login">
+            <input 
+            type="text" 
+            placeholder='Full name'
+            value="fullName"
+            className="input-login" />
+          </label>
+          <label className="label-login">
+            <input 
+            type="text" 
+            placeholder='Email'
+            value="email"
+            className="input-login" />
+          </label>
+          <label className="label-login">
+            <input 
+            type="text" 
+            placeholder='Phone Number'
+            value="phoneNumber"
+            className="input-login" />
+          </label>
+          <label className="label-login">
+            <input 
+            type="password" 
+            placeholder='Password'
+            value="password"
+            className="input-login" />
+          </label>
+          </form>
+      </div>
     </div>
   )
 }
