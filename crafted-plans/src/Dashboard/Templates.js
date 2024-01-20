b98a85165c625241f8249396a4f5604718448df6
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-20 21:54:32 +0200

    styled modals

--- crafted-plans/src/Dashboard/Templates.js
+++ crafted-plans/src/Dashboard/Templates.js
@@ -1,57 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { toast } from "react-hot-toast";
-
+import { toast } from 'react-hot-toast';
 
 const Templates = () => {
-  const {id} = useParams();
+  const { id } = useParams() || {};
   const [activeCategory, setActiveCategory] = useState('pages');
-  const [templates, setTemplates] = useState("");
-  const [price, setPrice] = useState("");
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("");
+  const [templates, setTemplates] = useState([]);
+  const [price, setPrice] = useState('');
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
   const [images, setImages] = useState([]);
+  const [imagesArray, setImagesArray] = useState([]);
   const [isAddProductModalOpen, setAddProductIsModalOpen] = useState(false);
+  const [updateModalOpen, setUpdateModalOpen] = useState(false);
 
   const handleAddProduct = () => {
+    setName('');
+    setCategory('');
+    setPrice('');
+    setImages([]);
     setAddProductIsModalOpen(true);
   };
+
+  const handleUpdateTemplate = (template) => {
+    setName(template.name);
+    setCategory(template.category);
+    setPrice(template.price);
+    setImagesArray(template.image);
+    setImages([]);
+    setUpdateModalOpen(true);
+  };
+
   const closeModal = () => {
     setAddProductIsModalOpen(false);
   };
+
+  const closeUpdateModal = () => {
+    setUpdateModalOpen(false);
+  };
+
   const getAllTemplatesByCategory = (category) => {
-    axios.post(`http://localhost:5000/templates/getTemplateByCategory`, { category })
+    axios
+      .post(`http://localhost:5000/templates/getTemplateByCategory`, { category })
       .then((response) => {
         setTemplates(response.data.templates);
-        console.log(response.data.templates);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+
   const handleCategoryChange = (category) => {
     setActiveCategory(category);
     getAllTemplatesByCategory(category);
   };
-  
+
   const handleDeleteTemplate = (id) => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this template?");
-    
+    const confirmDelete = window.confirm('Are you sure you want to delete this template?');
+
     if (confirmDelete) {
-      axios.delete(`http://localhost:5000/templates/deleteTemplate/${id}`)
+      axios
+        .delete(`http://localhost:5000/templates/deleteTemplate/${id}`)
         .then((response) => {
           console.log(`Template with ID ${id} deleted successfully`);
           getAllTemplatesByCategory(activeCategory);
-          toast.success('Template deleted successfully!', { position: "top-center" });
+          toast.success('Template deleted successfully!', { position: 'top-center' });
         })
         .catch((error) => {
-          console.error("Error deleting template", error);
+          console.error('Error deleting template', error);
           // Show error toast
-          toast.error('Error deleting template. Please try again.', { position: "top-center" });
+          toast.error('Error deleting template. Please try again.', { position: 'top-center' });
         });
     }
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Create FormData object
@@ -63,7 +86,8 @@ const Templates = () => {
     for (let i = 0; i < images.length; i++) {
       formData.append('images', images[i]);
     }
-    axios.post('http://localhost:5000/templates/addTemplate', formData)
+    axios
+      .post('http://localhost:5000/templates/addTemplate', formData)
       .then((response) => {
         console.log(response.data);
         setAddProductIsModalOpen(false);
@@ -75,98 +99,155 @@ const Templates = () => {
         toast.error('Error adding template. Please try again.', { position: 'bottom-right' });
       });
   };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('category', category);
+    formData.append('price', price);
+
+    // Append each selected image to the form data
+    for (let i = 0; i < images.length; i++) {
+      formData.append('images', images[i]);
+    }
+
+    // Append previous images to the form data
+    for (let i = 0; i < imagesArray.length; i++) {
+      formData.append('imagesArray', imagesArray[i]);
+    }
+
+    axios
+      .put(`http://localhost:5000/templates/updateTemplate/${id}`, formData)
+      .then((response) => {
+        console.log(response.data);
+        setUpdateModalOpen(false);
+        getAllTemplatesByCategory(activeCategory);
+        toast.success('Template updated successfully!', { position: 'bottom-right' });
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error('Error updating template. Please try again.', { position: 'bottom-right' });
+      });
+  };
+
   useEffect(() => {
     getAllTemplatesByCategory(activeCategory);
   }, [activeCategory]);
 
   return (
     <div>
-    <div className='products-D'>
-      <div className='products-toggle-D'>
-        <button
-          className={`products-btns-D ${activeCategory === 'pages' ? 'active' : ''}`}
-          onClick={() => handleCategoryChange('pages')}
-        >
-          Pages
-        </button>
-        <button
-          className={`products-btns-D ${activeCategory === 'addOns' ? 'active' : ''}`}
-          onClick={() => handleCategoryChange('addOns')}
-        >
-          Add Ons
-        </button>
-        <button
-          className={`products-btns-D ${activeCategory === 'cover' ? 'active' : ''}`}
-          onClick={() => handleCategoryChange('cover')}
-        >
-          Covers
-        </button>
-      </div>
-      <div className='products-container-D'>
-        <button 
-        className='add-product-D'
-        onClick={handleAddProduct}
-        >Add</button>
-        <hr className='division-line-D' />
-        <div className='show-products-D'>
-          {/* Render templates based on the active category */}
-          {templates && templates.map((template) => (
-            <div key={template.id} className='product-card-D'>
-              <img
-                src={template.image}
-                alt={template.name}
-                className='product-image-D'
-              />
-              <div className='edit-product-D'>
-                <img
-                  src="/Images/pen.svg"
-                  alt='edit-image-1'
-                  className='product-image-D1'
-                />
-                <img
-                  src="/Images/bin.svg"
-                  alt='edit-image-2'
-                  className='product-image-D1'
-                  onClick={() => handleDeleteTemplate(template._id)}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-     {/* Modal for adding a template */}
-    {isAddProductModalOpen && (
-      <div className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={closeModal}>&times;</span>
-          <form 
-          onSubmit={handleSubmit}
+      <div className="products-D">
+        <div className="products-toggle-D">
+          <button
+            className={`products-btns-D ${activeCategory === 'pages' ? 'active' : ''}`}
+            onClick={() => handleCategoryChange('pages')}
           >
-            <label>
-              Name:
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
-              Category:
-              <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
-            </label>
-            <label>
-              Price:
-              <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
-            </label>
-            <label>
-              Images (up to 3):
-              <input type="file" multiple onChange={(e) => setImages(e.target.files)} />
-            </label>
-            <button type="submit">Submit</button>
-          </form>
+            Pages
+          </button>
+          <button
+            className={`products-btns-D ${activeCategory === 'addOns' ? 'active' : ''}`}
+            onClick={() => handleCategoryChange('addOns')}
+          >
+            Add Ons
+          </button>
+          <button
+            className={`products-btns-D ${activeCategory === 'cover' ? 'active' : ''}`}
+            onClick={() => handleCategoryChange('cover')}
+          >
+            Covers
+          </button>
+        </div>
+        <div className="products-container-D">
+          <button className="add-product-D" onClick={handleAddProduct}>
+            Add
+          </button>
+          <hr className="division-line-D" />
+          <div className="show-products-D">
+            {/* Render templates based on the active category */}
+            {templates &&
+              templates.map((template) => (
+                <div key={template.id} className="product-card-D">
+                  <img src={template.image} alt={template.name} className="product-image-D" />
+                  <div className="edit-product-D">
+                    <img
+                      src="/Images/pen.svg"
+                      alt="edit-image-1"
+                      className="product-image-D1"
+                      onClick={() => handleUpdateTemplate(template)}
+                    />
+                    <img
+                      src="/Images/bin.svg"
+                      alt="edit-image-2"
+                      className="product-image-D1"
+                      onClick={() => handleDeleteTemplate(template._id)}
+                    />
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-    )}
-    
-  </div>
-);
+      {/* Modal for adding a template */}
+      {isAddProductModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <form className='form-modal-dashboard' onSubmit={handleSubmit}>
+              <label className='label-modal-dashboard'>
+                Name:
+                <input className='input-form-dashboard' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Category:
+                <input className='input-form-dashboard' type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Price:
+                <input className='input-form-dashboard' type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Images (up to 3):
+                <input className='input-form-dashboard' type="file" multiple onChange={(e) => setImages(e.target.files)} />
+              </label>
+              <button className='submit-form-dashboard' type="submit">Add</button>
+            </form>
+          </div>
+        </div>
+      )}
+      {/* Modal for updating a template */}
+      {updateModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeUpdateModal}>
+              &times;
+            </span>
+            <form className='form-modal-dashboard' onSubmit={handleUpdate}>
+              <label className='label-modal-dashboard'>
+                Name:
+                <input className='input-form-dashboard' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Category:
+                <input className='input-form-dashboard' type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Price:
+                <input className='input-form-dashboard' type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+              </label>
+              <label className='label-modal-dashboard'>
+                Images (up to 3):
+                <input className='input-form-dashboard' type="file" multiple onChange={(e) => setImages(e.target.files)} />
+              </label>
+              <button className='submit-form-dashboard' type="submit">Update</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default Templates
+export default Templates;
