f553b7ade3ab7fdeae7a92079aadb0b0887cd772
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-21 22:22:12 +0200

    done with homePage responsive

--- crafted-plans/src/HomePage/Gallery.jsx
+++ crafted-plans/src/HomePage/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
     <div className='gallery-container-home'>
       <div className="gallery-home">
   <div className="item long-picture-home">
-    <img src="/Images/gallery1.png" alt="gallery1" />
+    <img className='image-gallery1' src="/Images/gallery1.png" alt="gallery1" />
   </div>
   <div className="item top-left-home">
     <img src="/Images/gallery2.png" alt="gallery2" />
@@ -15,7 +15,7 @@ const Gallery = () => {
     <img src="/Images/gallery3.png" alt="gallery3" />
   </div>
   <div className="item bottom-left-home">
-    <img src="/Images/gallery4.png" alt="gallery4" />
+    <img className='image-gallery' src="/Images/gallery4.png" alt="gallery4" />
   </div>
 </div>
     </div>
