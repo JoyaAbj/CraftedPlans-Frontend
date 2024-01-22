9b2a39cf1defd464167136deb5ef286323517ab1
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-22 22:02:33 +0200

    setLoader for covers

--- crafted-plans/src/Planner/Cover.jsx
+++ crafted-plans/src/Planner/Cover.jsx
@@ -1,98 +1,119 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Styles/planner.css';
 import axios from 'axios';
 
 const Cover = () => {
   const [inputValue, setInputValue] = useState('');
-  const [selectedCover, setSelectedCover] = useState(null); 
+  const [selectedCover, setSelectedCover] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isMonthModalOpen,setIsMonthModalOpen] = useState(false);
+  const [isMonthModalOpen, setIsMonthModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Submitted value:', inputValue);
   };
 
-
-// Fetch covers
+  // Fetch covers
   const [cover, setCover] = useState([]);
   const category = "cover";
-  // const url = process.env.REACT_APP_API_URL;
-  const getAllCovers = () => {
-      axios.post(`http://localhost:5000/templates/getTemplateByCategory`, {category})
-        .then((response) => {
-          setCover(response.data.templates);
-          console.log(response.data.templates)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-    
-    useEffect(() => {
-      getAllCovers();
-    }, []);
 
-    const handleProductClick = (i) => {
-      setSelectedCover(cover[i]);
-      setIsModalOpen(true); 
-    };
-  
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
-    const handleOpenMonthModal = () => {
-      setIsMonthModalOpen(true); 
-    }
-    const handleChooseCover = (selectedCover) =>{
-      localStorage.setItem('cover', selectedCover._id)
-    }
-    
+  const getAllCovers = () => {
+    axios.post(`http://localhost:5000/templates/getTemplateByCategory`, { category })
+      .then((response) => {
+        setCover(response.data.templates);
+        // setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+          setShowContent(true);
+        }, 4000);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  };
+
+  useEffect(() => {
+    getAllCovers();
+  }, [isLoading]);
+
+  const handleProductClick = (i) => {
+    setSelectedCover(cover[i]);
+    setIsLoading(true);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOpenMonthModal = () => {
+    setIsMonthModalOpen(true);
+  };
+
+  const handleChooseCover = (selectedCover) => {
+    localStorage.setItem('cover', selectedCover._id);
+  };
+
   return (
     <div>
       {/* Covers */}
-      <div className="cover-card-cover" >
-      {cover && cover.map ((cover, i) =>(
-       <div>
-       <div className="image-cover" key={i}>
-            <img 
-            src={cover.image} 
-            alt="product" 
-            className="img-cover"
-            onClick={() => handleProductClick(i)} 
-            />
-        </div>
-        <p className="name-cover">
-            {cover.name}
-        </p>
-        </div>
-    ))}
-    </div>
-     {/* Modal */}
-     {isModalOpen && selectedCover && (
+      <div className="cover-card-cover">
+        {cover && cover.map((cover, i) => (
+          <div key={i}>
+            <div className="image-cover" key={i}>
+              <img
+                src={cover.image}
+                alt="product"
+                className="img-cover"
+                onClick={() => handleProductClick(i)}
+              />
+            </div>
+            <p className="name-cover">
+              {cover.name}
+            </p>
+          </div>
+        ))}
+      </div>
+      {/* Modal */}
+      {isModalOpen && selectedCover && (
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={closeModal}>&times;</span>
-            <div className="cover-images-modal">
-              <img src={selectedCover.image} alt="product" className="img-cover-modal" />
-              <img src={selectedCover.image[1]} alt="product" className="img-cover-modal" />
-              <img src={selectedCover.image[2]} alt="product" className="img-cover-modal" />
-            </div>
-            <p className="name-cover-modal">{selectedCover.name}</p>
-            <p className="name-cover-modal">{selectedCover.price}$</p>
-            <button 
-            className="choose-cover-modal"
-            onClick={()=>handleChooseCover(selectedCover)}>Choose This Cover</button>
-            <div className="month-view">
-            <p className="name-cover-modal1">
-              Every planner includes a cover page and a month at a glance
-              </p>
-              <img 
-              src="/TopBar/events.png" 
-              alt="month at a glance" 
-              className="month-at-a-glance" 
-              onClick={handleOpenMonthModal}/>
-            </div>
-            {console.log("Modal content displayed")}
+            {isLoading ? (<img
+              className='loader'
+              src="/Images/wired-outline-112-book.gif"
+              alt="loader" />
+            ) : showContent && (
+              <div>
+                <div className="cover-images-modal">
+                  <img src={selectedCover.image} alt="product" className="img-cover-modal" />
+                  <img src={selectedCover.image[1]} alt="product" className="img-cover-modal" />
+                  <img src={selectedCover.image[2]} alt="product" className="img-cover-modal" />
+                </div>
+                <p className="name-cover-modal">{selectedCover.name}</p>
+                <p className="name-cover-modal">{selectedCover.price}$</p>
+                <button
+                  className="choose-cover-modal"
+                  onClick={() => handleChooseCover(selectedCover)}
+                >
+                  Choose This Cover
+                </button>
+                <div className="month-view">
+                  <p className="name-cover-modal1">
+                    Every planner includes a cover page and a month at a glance
+                  </p>
+                  <img
+                    src="/TopBar/events.png"
+                    alt="month at a glance"
+                    className="month-at-a-glance"
+                    onClick={handleOpenMonthModal}
+                  />
+                </div>
+                {console.log("Modal content displayed")}
+              </div>
+            )}
           </div>
         </div>
       )}
